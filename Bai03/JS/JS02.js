b477836b70492c7f21946a73a1a7f0b1f8f443ea
Nguyễn Thành Long <104711708+AIMH9999@users.noreply.github.com>
2023-05-27 20:03:47 +0700

    Update JS02.js

--- Bai03/JS/JS02.js
+++ Bai03/JS/JS02.js
@@ -1,7 +1,6 @@
-
 function timGiaTriLonNhat(a) {
   let max = a[0];
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < a.length; i++) { 
     if (max < a[i]){
       max = a[i];
     }      
@@ -10,4 +9,3 @@ function timGiaTriLonNhat(a) {
 }
 // Ví dụ sử dụng
 console.log(timGiaTriLonNhat([5, 8, 3, 2, 9])); // Kết quả: 9
-
